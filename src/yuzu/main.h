76409f9e563d023ffe89ae981d13749c7430d515
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2024-03-07 08:54:49 +0000

    Update main.h

--- src/yuzu/main.h
+++ src/yuzu/main.h
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2014 Citra Emulator Project & 2024 suyu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+// Modified by palfaiate on <2024/03/07>
+
 #pragma once
 
 #include <memory>
