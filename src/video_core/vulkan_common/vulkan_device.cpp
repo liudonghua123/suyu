556d26256640654e392da2a13df27467d314449c
Alessio1989 <alessiot89@users.noreply.github.com>
2024-03-12 04:22:32 +0100

    dynamic state 3 blending should work correctly in AMD proprietary drivers now

--- src/video_core/vulkan_common/vulkan_device.cpp
+++ src/video_core/vulkan_common/vulkan_device.cpp
@@ -594,13 +594,13 @@ Device::Device(VkInstance instance_, vk::PhysicalDevice physical_, VkSurfaceKHR 
             dynamic_state3_enables = false;
         }
     }
-    if (extensions.extended_dynamic_state3 && is_amd_driver) {
-        LOG_WARNING(Render_Vulkan,
-                    "AMD drivers have broken extendedDynamicState3ColorBlendEquation");
-        features.extended_dynamic_state3.extendedDynamicState3ColorBlendEnable = false;
-        features.extended_dynamic_state3.extendedDynamicState3ColorBlendEquation = false;
-        dynamic_state3_blending = false;
-    }
+    //if (extensions.extended_dynamic_state3 && is_amd_driver) {
+    //    LOG_WARNING(Render_Vulkan,
+    //                "AMD drivers have broken extendedDynamicState3ColorBlendEquation");
+    //    features.extended_dynamic_state3.extendedDynamicState3ColorBlendEnable = false;
+    //    features.extended_dynamic_state3.extendedDynamicState3ColorBlendEquation = false;
+    //    dynamic_state3_blending = false;
+    //}
     if (extensions.vertex_input_dynamic_state && is_radv) {
         // TODO(ameerj): Blacklist only offending driver versions
         // TODO(ameerj): Confirm if RDNA1 is affected
