62f36aceb7a4938c4a3c9c3bfe066b91e2da497c
Paulo Alfaiate <syl.paulo.alfaiate@gmail.com>
2024-03-18 17:10:30 +0000

    Update key_manager.cpp

--- src/core/crypto/key_manager.cpp
+++ src/core/crypto/key_manager.cpp
@@ -669,10 +669,14 @@ static bool ValidCryptoRevisionString(std::string_view base, size_t begin, size_
 void KeyManager::LoadFromFile(const std::filesystem::path& file_path, int key_type) {
     if (!Common::FS::Exists(file_path) && key_type == 1) {
         switch (key_type) {
-            case 1: LOG_ERROR(Crypto, "Issue with Development key file at '{}': File not found", file_path.generic_string());
-            case 2: LOG_ERROR(Crypto, "Issue with Production key file at '{}': File not found", file_path.generic_string());
-            case 3: LOG_INFO(Crypto, "Issue with Title key file at '{}': File not found", file_path.generic_string());
-            case 4: LOG_INFO(Crypto, "Issue with Console key file at '{}': File not found", file_path.generic_string());
+            case 1: LOG_ERROR(Crypto, "Issue with Development key file at '{}': File not found",
+                              file_path.generic_string());
+            case 2: LOG_ERROR(Crypto, "Issue with Production key file at '{}': File not found", 
+                              file_path.generic_string());
+            case 3: LOG_INFO(Crypto, "Issue with Title key file at '{}': File not found", 
+                             file_path.generic_string());
+            case 4: LOG_INFO(Crypto, "Issue with Console key file at '{}': File not found", 
+                             file_path.generic_string());
             default: LOG_ERROR(Crypto, "Unknown Key Type");
         }
         return;
