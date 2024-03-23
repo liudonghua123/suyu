d535e826aa61f35d9b1b38bd2de2ba24b05a3d59
Downloafy <downloafy@noreply.localhost>
2024-03-23 22:40:53 +0100

    copyright
    might get copyrighted if not changed

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 suyu Emulator Project
+// SPDX-FileCopyrightText: 2024 suyu Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import android.annotation.SuppressLint
