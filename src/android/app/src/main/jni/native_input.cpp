89c24604f248fa31f1022665105992fa1f289c83
SylverDiscord51 <p71468162@gmail.com>
2024-03-24 01:14:49 +0000

    Fix Copyright

--- src/android/app/src/main/jni/native_input.cpp
+++ src/android/app/src/main/jni/native_input.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 yuzu emulator project & 2024 suyu Emulator Project
+// SPDX-FileCopyrightText: 2023 yuzu emulator project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <common/fs/fs.h>
